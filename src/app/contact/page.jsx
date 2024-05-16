8af3459e97ea7059aa098f5c549681fb78e69ea7
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-05-16 16:03:01 +0300

    contact page top image edited

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -13,32 +13,25 @@ export async function generateMetadata({ params, searchParams }, parent) {
 export default async function Page() {
   return (
     <>
-      <Container>
-        <Row>
-          <Col xs={12} lg={12}>
-            <div
-              className="page-slide"
-              style={{
-                background:
-                  "url(../../../img/slider/slide-4.webp) center center no-repeat",
-                backgroundSize: "cover",
-              }}
-            >
-              <Container style={{ height: "100%", position: "relative" }}>
-                <div className="text-type-2">
-                  <h1>Contact</h1>
-                  <p>
-                    Please do not hesitate to contact us for your questions
-                    regarding boat, yacht charter &amp; Bosphorus Tours. One of
-                    our yacht specialist will be happy to guide you through
-                    booking.
-                  </p>
-                </div>
-              </Container>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <div
+        className="page-slide"
+        style={{
+          background:
+            "url(../../../img/slider/slide-4.webp) center center no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <Container style={{ height: "100%", position: "relative" }}>
+          <div className="text-type-2">
+            <h1>Contact</h1>
+            <p>
+              Please do not hesitate to contact us for your questions regarding
+              boat, yacht charter &amp; Bosphorus Tours. One of our yacht
+              specialist will be happy to guide you through booking.
+            </p>
+          </div>
+        </Container>
+      </div>
       <Contact classNames={"mt-90"} />
       <div className="ltn__call-to-action-area call-to-action-6 before-bg-bottom">
         <Container>
