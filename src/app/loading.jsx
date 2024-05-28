b33287e471e69f8b055f275228d8be2ae91d5b26
tevfikkdurmus <tevfikk.durmus@gmail.com>
2024-05-28 12:00:11 +0300

    loading component added

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div>loading..1</div>
+    <div style={{position: "absolute", backgroundColor: "gray",left: 0,right: 0,top: 0,bottom: 0 , zIndex: 1000, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <div className='loader'></div>
+    </div>
   )
 }
